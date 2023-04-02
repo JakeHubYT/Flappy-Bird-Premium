d5da440a871a59493f067aac2843937e351d6e35
JacobAMartin <121958112+JakeHubYT@users.noreply.github.com>
2023-04-02 17:35:57 -0500

    Organized Project

---
+++ Assets/_Scripts/FireWorkScript.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FireWorkScript : MonoBehaviour
+{
+    public ParticleSystem[] fireWork;
+    public AudioClip fireWorkSound;
+
+    private void OnEnable()
+    {
+        Actions.OnNewHighScore += StartCoroutine;
+    }
+    private void OnDisable()
+    {
+        Actions.OnNewHighScore -= StartCoroutine;
+
+    }
+
+    private void StartCoroutine()
+    {
+        StartCoroutine(ExecuteFunctionRandomly());
+    }
+
+    IEnumerator ExecuteFunctionRandomly()
+    {
+        float waitTime = Random.Range(0f, 1f);
+        yield return new WaitForSeconds(waitTime);
+
+        // Call your function here
+        LaunchFireWork();
+
+       // StartCoroutine(ExecuteFunctionRandomly());
+    }
+
+
+    void LaunchFireWork()
+    {
+
+
+        for (int i = 0; i < fireWork.Length; i++)
+        {
+            fireWork[i].Play();
+        }
+
+        AudioManager.Instance.PlaySound(fireWorkSound);
+    }
+}
